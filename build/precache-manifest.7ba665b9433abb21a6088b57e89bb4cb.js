4e1b253815402ec86443dcdf14d434666931e198
rajus0007 <ih15436@gmail.com>
2020-09-26 12:48:26 +0600

    third commit

---
+++ build/precache-manifest.7ba665b9433abb21a6088b57e89bb4cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1b531f9a000311a9d6dd97df258f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a16643e1510efb41fa9c",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "bdaa05f94f65f048f153",
+    "url": "/static/js/2.b6324373.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.b6324373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16643e1510efb41fa9c",
+    "url": "/static/js/main.5ae1749d.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
